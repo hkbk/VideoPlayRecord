1e21b979ce811bfe3054026a937ce3e4eed21ca2
vfa-hungnt <admin@admins-iMac-8.local>
2014-05-30 09:34:23 +0700

    define
    add file define

---
+++ Define.h
@@ -0,0 +1,16 @@
+//
+//  Define.h
+//  VideoPlayRecord
+//
+//  Created by HungNT on 5/30/14.
+//  Copyright (c) 2014 HungNT. All rights reserved.
+//
+
+#ifndef VideoPlayRecord_Define_h
+#define VideoPlayRecord_Define_h
+
+#define buttonColor [UIColor grayColor]
+#define backgroundColor [UIColor blackColor]
+
+
+#endif
